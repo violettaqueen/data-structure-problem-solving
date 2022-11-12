05e51b4a6465d9982cdce99549f52d1e61a32a8e
Violetta <violetta.v.queen@gmail.com>
2022-11-11 22:00:12 -0700

    two sum task

---
+++ src/main/java/arrays_arraylist/TwoSum.java
@@ -0,0 +1,16 @@
+package arrays_arraylist;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int [] nums = {2, 7, 11, 15};
+        int target = 9;
+
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i] + nums[i+1] == target){
+//                System.out.println(nums[i] +", "+ nums[i+1]);
+//            }
+//        }
+    }
+}
---
+++ src/main/java/linkedLists/Main.java
@@ -0,0 +1,4 @@
+package linkedLists;
+
+public class Main {
+}
