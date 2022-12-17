37b238900032eca31fe54a82c1551fb37d7bb0d5
Violetta <violetta.v.queen@gmail.com>
2022-12-17 12:01:52 -0700

    move all zeros to the end

---
+++ src/main/java/arrays_arraylist/MoveAllZerosToTheEnd.java
@@ -0,0 +1,27 @@
+package arrays_arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MoveAllZerosToTheEnd {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,0,3,0,2,0,1));
+        int originalSize = list.size();
+        list.removeAll(Arrays.asList(0));
+        int newSize = list.size();
+        int totalNumberOfZeros = originalSize - newSize;
+
+        for (int i = 0; i < totalNumberOfZeros; i++) {
+            list.add(0);
+        }
+        System.out.println(list);
+
+
+
+
+
+    }
+}
