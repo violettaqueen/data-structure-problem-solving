8ea5e82b92de09b4889ab152d62b507ea40f05ac
Violetta <violetta.v.queen@gmail.com>
2022-12-17 07:04:50 -0700

    Swap two numbers

---
+++ src/main/java/tasks/SwapTwoNumbers.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 7;
+
+        int temp = num2;
+        num2 = num1;
+        num1 = temp;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+
+
+
+    }
+}
