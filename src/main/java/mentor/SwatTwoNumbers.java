c7646dcf827f308d7665e715194e146f11934dcc
Violetta <violetta.v.queen@gmail.com>
2022-12-17 07:04:50 -0700

    Swap two numbers

---
+++ src/main/java/mentor/SwatTwoNumbers.java
@@ -0,0 +1,19 @@
+package mentor;
+
+public class SwatTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 23;
+        int num2 = 45;
+
+        int temp = 45;
+         num2 = num1;
+         num1 = temp;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+    }
+}
